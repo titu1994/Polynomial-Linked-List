d9a184925b48d9cc7bb9b44f21ab3693a7ebfc1d
titu1994 <titu1994@gmail.com>
2013-09-20 21:53:50 +0530

    Finished implementation.
    Merge Sort Sibtract code is still to large, Merge Sort Multiply is still
    not finished.

--- Polynomial Linked List/src/Polynomial Linked List.c
+++ Polynomial Linked List/src/Polynomial Linked List.c
@@ -21,14 +21,15 @@ int main(void) {
 	int choice = -1, power, coefficient;
 	Node *p = NULL, *q = NULL, *r = NULL;
 
-	void addPoly(Node **, int coeff, int pow);
-	Node* mergeSortPoly(Node *, Node *);
+	void createPoly(Node **, int coeff, int pow);
+	Node* addPoly(Node *, Node *);
+	Node* multiplyPoly(Node*, Node*);
 	void displayPoly(Node *);
 	void listDestructor(Node **);
 
 	setbuf(stdout, NULL);
 
-	printf("Enter choice : \n1 for add to 1st polynomial\n2 for add to 2nd polynomial\n3 for calculate polynomial\n4 for display polynomial representation\n");
+	printf("Enter choice : \n1 for add to 1st polynomial\n2 for add to 2nd polynomial\n3 for calculate polynomial\n4 for display polynomial representation\n5 to multiply poly\n6 to delete all poly\n");
 
 	do{
 		printf("Enter choice : ");
@@ -40,7 +41,7 @@ int main(void) {
 				printf("Enter a coefficient and power\n");
 				scanf("%d%d", &coefficient, &power);
 
-				addPoly(&p, coefficient, power);
+				createPoly(&p, coefficient, power);
 
 				break;
 
@@ -50,14 +51,17 @@ int main(void) {
 				printf("Enter a coefficient and power\n");
 				scanf("%d%d", &coefficient, &power);
 
-				addPoly(&q, coefficient, power);
+				createPoly(&q, coefficient, power);
 
 				break;
 
 			case 3:
 				//Calculate Polynomial
 
-				r = mergeSortPoly(p, q);
+				//r = mergeSortPoly(p, q);
+
+				r = addPoly(p, q);
+
 
 				break;
 
@@ -69,7 +73,21 @@ int main(void) {
 				displayPoly(r);
 
 				break;
+			case 5:{
+				//Multiply 2 poly
 
+				r = multiplyPoly(p, q);
+
+				break;
+			}
+			case 6:{
+
+				listDestructor(&p);
+				listDestructor(&q);
+				listDestructor(&r);
+
+				break;
+			}
 			default:{
 				choice = -1;
 			}
@@ -87,30 +105,88 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
-void addPoly(Node **p, int coeff, int pow){
-	Node *newNode = NULL;
+void createPoly(Node **p, int coeff, int pow){
 
-	newNode = (Node*) calloc(1, sizeof(Node));
-	newNode->coefficient = coeff;
-	newNode->power = pow;
-	newNode->next = NULL;
+	Node *temp = *p, *previous;
 
-	if(*p == NULL){
-		*p = newNode;
+	while(temp != NULL){
+		if(temp->power <= pow){
+			break;
+		}
+
+		previous = temp;
+		temp = temp->next;
+	}
+
+	if(temp == NULL || temp->power != pow){
+		Node *newNode = NULL;
+		newNode = (Node*) calloc(1, sizeof(Node));
+		newNode->coefficient = coeff;
+		newNode->power = pow;
+
+		if(*p == temp){
+			newNode->next = *p;
+			*p = newNode;
+		}
+		else{
+			newNode->next = previous->next;
+			previous->next = newNode;
+		}
+
 	}
 	else{
-		Node *temp = *p;
-
-		while(temp->next != NULL){
-			temp = temp->next;
-		}
-		temp->next = newNode;
+		temp->coefficient += coeff;
 	}
 
 }
 
+Node* addPoly(Node *p, Node *q){
 
-Node* mergeSortPoly(Node *p, Node *q){
+	Node *result = NULL;
+
+	while(p != NULL){
+
+		createPoly(&result, p->coefficient, p->power);
+		p = p->next;
+
+	}
+
+	while(q != NULL){
+
+		createPoly(&result, q->coefficient, q->power);
+		q = q->next;
+
+	}
+
+	return result;
+
+}
+
+Node* multiplyPoly(Node *p, Node *q){
+
+	Node *t = q;
+	Node *result = NULL;
+
+	while(p != NULL){
+
+		while(q != NULL){
+
+			createPoly(&result, p->coefficient * q->coefficient, p->power + q->power);
+			q = q->next;
+
+		}
+
+		p = p->next;
+		q = t;
+
+	}
+
+	return result;
+
+}
+
+
+Node* mergeSortAddPoly(Node *p, Node *q){
 
 	Node *r = NULL;
 	Node *t;
@@ -169,6 +245,183 @@ Node* mergeSortPoly(Node *p, Node *q){
 		}
 		else if(q != NULL){
 
+			while(q != NULL){
+				r->next = (Node*) calloc(1, sizeof(Node));
+				r = r->next;
+				r->next = NULL;
+
+				r->power = q->power;
+				r->coefficient = q->coefficient;
+				q = q->next;
+
+			}
+
+		}
+
+	}
+
+	r->next = NULL;
+
+	return t;
+}
+
+
+Node* mergeSortSubtractPoly(Node *p, Node *q){
+
+	Node *r = NULL;
+	Node *t;
+
+	while(p != NULL || q != NULL){
+
+		if(p != NULL && q != NULL){
+
+			if(r == NULL){
+				r = (Node*) calloc(1, sizeof(Node));
+				t = r;
+
+			}
+			else{
+				r->next = (Node*) calloc(1, sizeof(Node));
+				r = r->next;
+			}
+
+			if(p->power < q->power){
+
+				r->power = p->power;
+				r->coefficient = p->coefficient;
+				p = p->next;
+
+			}
+			else if(p->power == q->power){
+
+				r->power = p->power;
+				r->coefficient = p->coefficient - q->coefficient;
+				p = p->next;
+				q = q->next;
+			}
+			else{
+
+				r->power = -q->power;
+				r->coefficient = -q->coefficient;
+				q = q->next;
+
+			}
+
+
+		}
+		else if(p != NULL){
+
+			while(p != NULL){
+
+				r->next = (Node*) calloc(1, sizeof(Node));
+				r = r->next;
+				r->next = NULL;
+
+				r->power = p->power;
+				r->coefficient = p->coefficient;
+				p = p->next;
+
+			}
+		}
+		else if(q != NULL){
+
+			while(q != NULL){
+				r->next = (Node*) calloc(1, sizeof(Node));
+				r = r->next;
+				r->next = NULL;
+
+				r->power = -q->power;
+				r->coefficient = -q->coefficient;
+				q = q->next;
+			}
+		}
+
+	}
+
+	r->next = NULL;
+
+	return t;
+}
+
+
+
+
+/*Node* mergeSortMultiplyPoly(Node *p, Node *q){
+	Node *r = NULL;
+	Node *t;
+
+	Node *result = NULL;
+
+	while(p != NULL){
+
+		while(q != NULL){
+			createPoly(&result, p->coefficient * q->coefficient, p->power + q->power);
+			q = q->next;
+
+		}
+
+		p = p->next;
+		q = t;
+
+	}
+
+	p = result;
+	q = result +
+
+	while(p != NULL || q != NULL){
+
+		if(p != NULL && q != NULL){
+
+			if(r == NULL){
+				r = (Node*) calloc(1, sizeof(Node));
+				t = r;
+
+			}
+			else{
+				r->next = (Node*) calloc(1, sizeof(Node));
+				r = r->next;
+			}
+
+			if(p->power < q->power){
+
+				r->power = p->power;
+				r->coefficient = p->coefficient;
+				p = p->next;
+
+			}
+			else if(p->power == q->power){
+
+				r->power = p->power + q->power;
+				r->coefficient = p->coefficient * q->coefficient;
+				p = p->next;
+				q = q->next;
+			}
+			else{
+
+				r->power = q->power;
+				r->coefficient = q->coefficient;
+				q = q->next;
+
+			}
+
+
+		}
+		else if(p != NULL){
+
+			while(p != NULL){
+
+				r->next = (Node*) calloc(1, sizeof(Node));
+				r = r->next;
+				r->next = NULL;
+
+				r->power = p->power;
+				r->coefficient = p->coefficient;
+				p = p->next;
+
+			}
+		}
+		else if(q != NULL){
+
 			r->next = (Node*) calloc(1, sizeof(Node));
 			r = r->next;
 			r->next = NULL;
@@ -185,6 +438,7 @@ Node* mergeSortPoly(Node *p, Node *q){
 
 	return t;
 }
+*/
 
 
 void displayPoly(Node *p){
